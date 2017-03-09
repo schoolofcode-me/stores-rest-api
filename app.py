bc184b8e26af35531fa080b32844a610dfd77522
Jose Salvatierra <jslvtr@gmail.com>
2017-03-09 21:46:03 +0000

    Modified app.py to create db tables too
    This is instead of just run.py creating the tables and relying
    on the WSGI app being ran by uWSGI or Heroku.
    
    It only creates the db tables if the app.config['DEBUG'] (also
    on app.py) is set to True. If it is set to False, it will only
    create db tables when executing run.py.

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+
+app.config['DEBUG'] = True
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
@@ -27,4 +30,10 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+
+    app.run(port=5000)
