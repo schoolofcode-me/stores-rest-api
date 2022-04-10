2652b7f40fed64c252ba52e36e306581843e49eb
Leonardo <leospairani@gmail.com>
2022-04-10 11:28:29 -0300

    Added db uri

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
